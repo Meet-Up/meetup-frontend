5f1750f5efcf94c11869a88e98f92950b41db91d
Daniel Perez <tuvistavie@gmail.com>
2013-09-22 17:28:25 +0900

    Start adding sass project.

--- Gruntfile.js
+++ Gruntfile.js
@@ -169,7 +169,10 @@ module.exports = function (grunt) {
         imagesDir: '<%= yeoman.app %>/images',
         javascriptsDir: '<%= yeoman.app %>/scripts',
         fontsDir: '<%= yeoman.app %>/styles/fonts',
-        importPath: '<%= yeoman.app %>/bower_components',
+        importPath: [
+          '<%= yeoman.app %>/bower_components',
+          '<%= yeoman.app %>/bower_components/compass-mixins/lib'
+        ],
         httpImagesPath: '/images',
         httpGeneratedImagesPath: '/images/generated',
         httpFontsPath: '/styles/fonts',
